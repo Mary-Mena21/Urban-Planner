c978695a6fddac4ca66c9f6923bc23c7593e92ec
Mariana Mena <maranashokry@ahoo.com>
2023-01-30 14:03:51 -0600

    Add project files.

---
+++ ConsoleApp3/Building.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planner
+{
+    internal class Building
+    {
+        /*---------Private Fields----------*/
+
+        private string _designer = "Mariana"; //field
+
+        private DateTime _dateConstructed;
+
+        private string _address;
+
+        private string _owner;
+
+
+
+
+        /*---------Public Properties----------*/
+
+        public string Designer
+        {
+            get {return  _designer; }
+            set { _designer = value; }
+        }
+
+        public string Address
+        {
+            get { return _address; }
+            //set { _address = value; }   //change the value
+        }
+
+        public string Owner
+        {
+            get { return _owner; }
+            set { _owner = value; }   //change the value
+        }
+        public DateTime DateConstructed
+        {
+            get { return _dateConstructed; }
+            //set { _address = value; }   //change the value
+        }
+
+        public int Stories
+        {
+            get;set;
+        }
+
+        public double Width
+
+        {
+            get; set;
+        }
+
+        public double Depth
+        {
+            get;set;
+        }
+
+        public double Volume
+        {
+            get {return Width*Depth*(3*Stories); }
+        }
+        /*---------Constructor-Method---------*/
+
+        public Building(string address)
+        {
+           this._address = address;
+
+        }
+        /*---------Methods---------*/
+
+        public void Construct()
+        {
+            this._dateConstructed = DateTime.Now;
+
+        }
+
+        public void Purchase(string owner)
+        {
+            this._owner = owner;
+        }
+
+
+
+        //public double VolumeValue(double Width, double Depth, int Stories)
+        //{
+        //    return Width * Depth * (Stories * 3);
+        //}
+    }
+}
+
+
+
+
+/*
+ 
+  private string name;
+  private int age;
+  //constructor
+  public User(string name, int age)
+  {
+    this.name = name;
+    this.age = age;
+  }
+  //getters
+  public String GetName() { return name; }
+  public int GetAge() { return age; }
+}
+ 
+
+ */
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,86 @@
+﻿using Planner;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(DateTime.Now.ToShortDateString());
+            //Console.WriteLine(DateTime.Now.ToShortTimeString());
+
+            /*--------------------------Test_1- working--------------------------*/
+            static void Method_1()
+            {
+                var buildings = new List<Building>();
+                Building building1 = new Building("512 8th Avenue");
+                building1.Depth = 1500;
+                building1.Width = 3;
+                building1.Stories = 4;
+                building1.Purchase("Mena");
+                building1.Construct();
+
+                //Building.ReferenceEquals(building1, building1);??????????
+
+                Building building2 = new Building("902 8th Avenue");
+                building2.Depth = 15;
+                building2.Width = 3;
+                building2.Stories = 4;
+                building2.Purchase("Mariana");
+                building1.Construct();
+
+                //building2.Address = "Test";//change the value
+
+                buildings.Add(building1);
+                buildings.Add(building2);
+
+
+                foreach (Building building in buildings)
+                {
+                    Console.WriteLine("Address :" + building.Address);
+                    Console.WriteLine("Designed by: " + building.Designer);
+                    Console.WriteLine("Owned by: " + building.Owner);
+                    Console.WriteLine("Constructed on: " + building.DateConstructed);
+                    Console.WriteLine("Volume " + building.Volume + " cubic meters of space");
+                    Console.WriteLine("----------------------------");
+                }
+
+            }
+            Method_1();
+            /*---------------------------Test_2- working--------------------------*/
+            static void Method_2()
+            {
+                var buildings = new List<Building>();
+                buildings.Add(new Building("512 8th Avenue") { Depth = 15, Stories = 3, Width = 3 , Owner="Mart"});
+                buildings.Add(new Building("902 9th Avenue") { Depth = 10, Stories = 5, Width = 7, Owner = "Mart2" });
+
+                foreach (Building building in buildings)
+                {
+                    Console.WriteLine("Address :" + building.Address);
+                    Console.WriteLine("Designed by: " + building.Designer);
+                    Console.WriteLine("Owned by: " + building.Owner);
+                    Console.WriteLine("Constructed on: " + building.DateConstructed);
+                    Console.WriteLine("Volume " + building.Volume + " cubic meters of space");
+                    Console.WriteLine("----------------------------");
+                }
+
+                //Console.WriteLine(buildings);
+            }
+            Method_2();
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
+//building.VolumeValue(building.Depth,building.Width,building.Stories);
+
+//building.Bulding(building._address);
+
+//building.Bulding(building._dateConstructed.ToShortDateString());
+
+
+//Building FiveOneTwoEigth = new Building("512 8th Avenue");
+//static void displayBuilding()
+//{
